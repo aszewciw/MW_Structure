161b16430bdda3c58e63c04b738f93af08f0581d
aszewciw <aszewci@gmail.com>
2016-12-07 13:55:19 -0600

    not sure what I'm adding

--- scripts/pair_count/bin_pair_indices_openmp.c
+++ scripts/pair_count/bin_pair_indices_openmp.c
@@ -30,6 +30,7 @@ Output:
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <omp.h>
 
@@ -41,37 +42,59 @@ typedef struct POINT{
     double weight;  /* chosen weight of correlation function */
 } POINT;
 
-/* info on pairs for different radial bins */
-typedef struct PAIRS{
+/* info on radial bins */
+typedef struct BINS{
     double r_lower, r_upper, r_middle, bin_size; /* bin dimensions */
     double r2_lower, r2_upper; /* squared bin edges */
-    double dd_raw;  /* raw pair counts */
-    double dd;      /* normalized pair counts */
+} BINS;
+
+/* info on pairs placed in corresponding bins of BINS structure */
+typedef struct PAIRS{
+    int * index1;    /* array of indices of first point in a pair */
+    int * index2;    /* array of indices of second point in a pair */
+    unsigned int N_pairs;   /* number of pairs for this bin */
 } PAIRS;
+/* -------------------------------------------------------------------------- */
+
+
 
 /* -------------------------------------------------------------------------- */
 
-void count_pairs( POINT *, int , PAIRS *, int );
-double norm_pairs( POINT *, int );
+/* concatenate two strings */
+char* concat(char *s1, char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
+    //in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
 
 /* -------------------------------------------------------------------------- */
+
 
 /* count raw weighted pairs for each bin */
-void count_pairs( POINT *data, int n_data, PAIRS *pairs, int N_bins ){
+void bin_pairs( POINT *data, int n_data, BINS *bins, int N_bins ){
+
+    /* temporary size of malloc'd arrays of pairs */
+    unsigned int n_temp = n_data * n_data / N_bins;
 
     #pragma omp parallel default(shared)
     {
-        double r1, r2, dx, dy, dz, ds2, w_i, w_j;
+        double r1, r2, dx, dy, dz, ds2;
         int i, j, k;
-        double counts_private[N_bins];
-        for(i=0; i<N_bins; i++) counts_private[i] = 0.0;
+        PAIRS *pairs = (PAIRS *)calloc(n_temp, sizeof(PAIRS));
+
+        for(i=0; i<N_bins; i++){
+            pairs[i].N_pairs = 0;
+            pairs[i].index1 = calloc(n_temp, sizeof(int));
+            pairs[i].index2 = calloc(n_temp, sizeof(int));
+        }
 
         #pragma omp for schedule(dynamic)
         for(i = 0; i < n_data; i++){
 
-            for(j = 0; j < n_data; j++){
-
-                if(i==j) continue;
+            for(j = i + 1; j < n_data; j++){
 
                 /* get component differences */
                 dx = data[i].x - data[j].x;
@@ -81,59 +104,42 @@ void count_pairs( POINT *data, int n_data, PAIRS *pairs, int N_bins ){
                 /* square of difference vector */
                 ds2 = dx * dx + dy * dy + dz * dz;
 
-                /* get weights */
-                w_i = data[i].weight;
-                w_j = data[j].weight;
-
                 /* assign pair to correct bin */
                 for(k = 0; k < N_bins; k++ ){
 
-                    r1 = pairs[k].r2_lower;
-                    r2 = pairs[k].r2_upper;
+                    r1 = bins[k].r2_lower;
+                    r2 = bins[k].r2_upper;
                     if(ds2 >= r1 && ds2 < r2){
                         /* add product to private counter */
-                        counts_private[k] += w_i * w_j;
+                        pairs[k].index1[N_pairs] = i;
+                        pairs[k].index2[N_pairs] = j;
+                        pairs[k].N_pairs += 1;
                         break;
                     }
                 }
             }
         }
         #pragma omp critical
-        for(k = 0; k < N_bins; k++) pairs[k].dd_raw += counts_private[k];
-    }
+        {
+            for(i = 0; i < N_bins; i++){
+                FILE *outfile;
+                    char * outfile = concat(fpath, fname);
 
-    int k;
-    for(k = 0; k < N_bins; k++) pairs[k].dd_raw /= 2.0;
-}
+                // strcpy(cl_args.filename, "../data/mcmc_output/mcmc_result.dat");
 
-/* -------------------------------------------------------------------------- */
+            }
 
-/* calculate normalization, the sum of the product of all weights */
-double norm_pairs( POINT *data, int n_data ){
-
-    double norm = 0.0;
-    int i, j;
-
-    #pragma omp parallel for default(shared) private(i,j) reduction(+:norm) \
-    schedule(dynamic)
-    for( i=0; i<n_data; i++ ){
-        for( j=0; j<n_data; j++ ){
-            if(i==j) continue;
-            norm += data[i].weight*data[j].weight;
         }
     }
 
-    /* Divide by 2 because of double counting */
-    norm /= 2.0;
-    return norm;
 }
 
 /* -------------------------------------------------------------------------- */
 
 int main(int argc, char **argv){
 
-    if (argc != 3){
-        fprintf( stderr, "Usage:\n %s data_file bins_file > outfile\n", argv[0]);
+    if (argc != 4){
+        fprintf( stderr, "Usage:\n %s data_file bins_file out_dir\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -144,32 +150,33 @@ int main(int argc, char **argv){
         exit(EXIT_FAILURE);
     }
 
-      FILE *bins_file;
+    FILE *bins_file;
 
     if((bins_file=fopen(argv[2],"r"))==NULL){
         fprintf(stderr,"Error: Cannot open file %s \n", argv[2]);
         exit(EXIT_FAILURE);
     }
 
+    /* path to output directory */
+    char * out_dir = argv[3];
+
     /* first read in number of bins */
     int n_bins;
     fscanf(bins_file, "%d", &n_bins);
 
-    PAIRS *pairs;
-    pairs = calloc(n_bins, sizeof(PAIRS));
+    BINS *bins;
+    bins = calloc(n_bins, sizeof(BINS));
 
     int i, k;
 
-    /*  read in bin settings and prepare corr */
+    /*  read in bin settings */
     for(k = 0; k < n_bins; k++){
-        fscanf(bins_file, "%lf", &pairs[k].r_lower);
-        fscanf(bins_file, "%lf", &pairs[k].r_upper);
-        fscanf(bins_file, "%lf", &pairs[k].r_middle);
-        fscanf(bins_file, "%lf", &pairs[k].bin_size);
-        pairs[k].r2_lower = pairs[k].r_lower * pairs[k].r_lower;
-        pairs[k].r2_upper = pairs[k].r_upper * pairs[k].r_upper;
-        pairs[k].dd_raw = 0.0;
-        pairs[k].dd = 0.0;
+        fscanf(bins_file, "%lf", &bins[k].r_lower);
+        fscanf(bins_file, "%lf", &bins[k].r_upper);
+        fscanf(bins_file, "%lf", &bins[k].r_middle);
+        fscanf(bins_file, "%lf", &bins[k].bin_size);
+        bins[k].r2_lower = bins[k].r_lower * bins[k].r_lower;
+        bins[k].r2_upper = bins[k].r_upper * bins[k].r_upper;
     }
 
     fclose(bins_file);
@@ -201,18 +208,17 @@ int main(int argc, char **argv){
     /* calculate the correlation */
     fprintf(stderr, "Start calculating mock pair counts... \n");
 
-    double normalization = norm_pairs(data, n_data);
-    count_pairs(data, n_data, pairs, n_bins);
+    bin_pairs(data, n_data, bins, n_bins, out_dir);
 
     /* output to file */
-    fprintf(stdout, "# r_lower, r_upper, r_middle, bin_size, dd, dd_raw, norm\n");
-    for(k = 0; k < n_bins; k++){
-        /* get normalized pair counts */
-        pairs[k].dd = pairs[k].dd_raw/normalization;
-        fprintf(stdout, "%lf\t%lf\t%lf\t%lf\t%le\t%le\t%le\n",
-            pairs[k].r_lower, pairs[k].r_upper, pairs[k].r_middle, pairs[k].bin_size,
-            pairs[k].dd, pairs[k].dd_raw, normalization);
-    }
+    // fprintf(stdout, "# r_lower, r_upper, r_middle, bin_size, dd, dd_raw, norm\n");
+    // for(k = 0; k < n_bins; k++){
+    //     /* get normalized pair counts */
+    //     pairs[k].dd = pairs[k].dd_raw/normalization;
+    //     fprintf(stdout, "%lf\t%lf\t%lf\t%lf\t%le\t%le\t%le\n",
+    //         pairs[k].r_lower, pairs[k].r_upper, pairs[k].r_middle, pairs[k].bin_size,
+    //         pairs[k].dd, pairs[k].dd_raw, normalization);
+    // }
 
     fprintf(stderr, "Done calculation and output. \n");
 
