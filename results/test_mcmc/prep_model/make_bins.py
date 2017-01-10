f883bd57518a7fd76234de755c901ca66d8c018b
aszewciw <aszewci@gmail.com>
2017-01-10 15:38:23 -0600

    adding line to make empty dir

--- results/test_mcmc/prep_model/make_bins.py
+++ results/test_mcmc/prep_model/make_bins.py
@@ -3,7 +3,7 @@ Make a set of distance bins. Here we use defaults settings.
 '''
 import mw_utilities_python as mwu
 import numpy as np
-import sys
+import sys, os
 
 def main():
 
@@ -13,6 +13,11 @@ def main():
     N_args          = len(args_array)
     assert(N_args == elements_needed)
     out_dir = args_array[1]
+
+    if not os.path.isdir(out_dir):
+        sys.stderr.write('{} does not exist Making directory...\n'.format(out_dir))
+        cmd = 'mkdir ' + out_dir
+        os.system(cmd)
 
     # Make default radial bins
     mwu.corr_prep.set_rbins(filepath=out_dir)
