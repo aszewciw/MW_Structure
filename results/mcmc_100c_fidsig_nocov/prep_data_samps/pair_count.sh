f2fbc73da0195f81226b5e27ffe1428033c37670
aszewciw <aszewci@gmail.com>
2017-01-23 19:17:22 -0600

    pair counting files

---
+++ results/mcmc_100c_fidsig_nocov/prep_data_samps/pair_count.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+cfname='pairs_cmd.txt';
+out_dir='./data/';
+mocks_dir='./data/';
+Nmocks=100;
+
+rm $cfname
+rm -rf $out_dir
+mkdir $out_dir
+
+python make_bins.py $out_dir
+python pair_count.py $cfname $mocks_dir $out_dir $Nmocks
+bash $cfname
+
+# Add information about run
+info_file='pairs_info.txt';
+rm $info_file
+INFO="pair_count.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
