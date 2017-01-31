8708f25cf065b8181c375a0353fc6a70fb262df0
aszewciw <aszewci@gmail.com>
2017-01-31 17:49:52 -0600

    make bins

--- results/mcmc_100chains/prep_bins/make_bins.sh
+++ results/mcmc_100chains/prep_bins/make_bins.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
-cfname='binned_cmd.txt';
-out_dir='./data/';
+cfname='bins_cmd.txt';
+out_dir='./';
+Nmocks=100;
 
 rm $cfname
 
 python make_bins.py $out_dir
-python bin_pair_indices.py $cfname $out_dir
 bash $cfname
 
 # Add information about run
-info_file='binned_info.txt';
+info_file='bins_info.txt';
 rm $info_file
-INFO="bin_pairs.sh was most recently run on:";
+INFO="make_bins.sh was most recently run on:";
 echo $INFO > $info_file
 date >> $info_file
 
