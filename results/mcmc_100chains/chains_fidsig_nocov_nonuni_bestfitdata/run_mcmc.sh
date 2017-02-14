0df00b5dec3b504d2f39754a93de8d18c8f0465c
aszewciw <aszewci@gmail.com>
2017-02-14 11:29:48 -0600

    median plotting

---
+++ results/mcmc_100chains/chains_fidsig_nocov_nonuni_bestfitdata/run_mcmc.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+cfname='mcmc_cmd.txt';
+indir='./data/';
+datdir='../prep_data_samps/data/';
+outdir='./out_data/';
+nprocs=16;
+max_s=100000;
+tol=0.0;
+Ndata=100;
+
+rm $cfname
+
+python run_mcmc.py $cfname $indir $datdir $outdir $nprocs $max_s $tol $Ndata
+bash $cfname
+
+# Add information about run
+info_file='mcmc_info.txt';
+rm $info_file
+INFO="run_mcmc.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
