491e691345ef2c251f684270af36e02127216b27
aszewciw <aszewci@gmail.com>
2017-03-12 18:47:24 -0500

    new files

---
+++ results/error_analysis/nmocks/compare_sigmas/calc_errors.py
@@ -0,0 +1,48 @@
+'''
+For each of our 1000 mocks, perform pair counting and save a file.
+'''
+import mw_utilities_python as mwu
+import sys, pickle, os
+import numpy as np
+
+def main():
+
+    # get command filename, out_dir
+    args_array = np.array(sys.argv)
+    N_args = len(args_array)
+    if N_args < 6:
+        sys.stderr.write("Not enough arguments were passed.\n")
+        sys.exit()
+    cfname    = args_array[1]
+    pairs_dir = args_array[2]
+    out_dir   = args_array[3]
+    nsubsets  = int(args_array[4])
+    nmocks_array = []
+    for i in range(nsubsets):
+        nmocks_array.append(args_array[4+i])
+
+    # get directories of scripts, executables, and star files
+    cleaned_dir = mwu.get_path.get_cleandata_path()
+    scripts_dir = mwu.get_path.get_scripts_path()
+    exe_file    = scripts_dir + 'errors/real_errors.py'
+
+    # Check for dir/file existence
+    if not os.path.isdir(out_dir):
+        sys.stderr.write('{} does not exist. Making directory...\n'.format(out_dir))
+        cmd = 'mkdir ' + out_dir
+        os.system(cmd)
+
+    if not os.path.isdir(pairs_dir):
+        sys.stderr.write('{} does not exist. Exiting...\n'.format(pairs_dir))
+        sys.exit()
+
+    # Write command file
+    with open(cfname, 'w') as f:
+        for n in nmocks_array:
+            cmd = ( 'python ' + exe_file + ' ' + cleaned_dir + ' ' + pairs_dir + ' '
+                + out_dir + ' ' + n )
+            f.write(cmd)
+            f.write('\n')
+
+if __name__ == '__main__':
+    main()
---
+++ results/error_analysis/nmocks/compare_sigmas/calc_errors.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+cfname=errors_cmd.txt;
+pairs_dir=./data_pairs/;
+out_dir=./data_errors/;
+nmocks='100 200 500 1000 2000 5000 10000';
+arraylen=6;
+
+rm $cfname
+
+python calc_errors.py $cfname $pairs_dir $out_dir $arraylen $nmocks
+bash $cfname
+
+# Add information about run
+info_file=errors_info.txt;
+rm $info_file
+INFO="calc_errors.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
