18699e10291720624d69d5d144a51f7ddbdf492f
aszewciw <aszewci@gmail.com>
2017-03-12 17:58:44 -0500

    making 10K mocks

---
+++ results/error_analysis/nmocks/prep_10000_mocks/make_mocks.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+cfname=mocks_cmd.txt;
+out_dir=../../data/;
+nprocs=8;
+nmocks=10000;
+rn=2.027;
+zn=0.234;
+rk=2.397;
+zk=0.675;
+a=0.053;
+
+rm $cfname
+rm -rf $out_dir
+mkdir $out_dir
+
+python make_mocks.py $cfname $out_dir $nprocs $nmocks $rn $zn $rk $zk $a
+bash $cfname
+
+# Add information about run
+info_file=mocks_info.txt;
+rm $info_file
+INFO="make_mocks.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
