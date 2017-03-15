d264c10764e5ac1d59d41d66822b79cbeba60084
aszewciw <aszewci@gmail.com>
2017-03-15 18:10:49 -0500

    jij

--- results/mcmc_100chains/prep_jk_errors/jk_mocks.sh
+++ results/mcmc_100chains/prep_jk_errors/jk_mocks.sh
@@ -5,4 +5,4 @@ Njk=10;
 mock_dir=/fs1/szewciw/MW_Structure/results/mcmc_100chains/prep_data_samps/data/;
 out_dir=./data/;
 
-python jk_mock.py $Nsamp $Njk $mock_dir $out_dir
+python jk_mocks.py $Nsamp $Njk $mock_dir $out_dir
