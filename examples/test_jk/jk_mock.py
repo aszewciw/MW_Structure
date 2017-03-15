554343623e4ac6a0812d8b8d4767b14c897883c7
aszewciw <aszewci@gmail.com>
2017-03-15 17:48:50 -0500

    ijji

--- examples/test_jk/jk_mock.py
+++ examples/test_jk/jk_mock.py
@@ -7,7 +7,7 @@ import numpy as np
 def main():
 
     # get command filename, out_dir, nprocs, nmocks, params
-    elements_needed = int(3)
+    elements_needed = int(4)
     args_array = np.array(sys.argv)
     N_args     = len(args_array)
     assert(N_args == elements_needed)
