37b03f0a7852d5538b591815b136d2fdd40ff1d6
aszewciw <aszewci@gmail.com>
2017-03-16 15:56:59 -0500

    rm line to print dir

--- scripts/mcmc/mcmc_prep/prepare_files_multi_data.py
+++ scripts/mcmc/mcmc_prep/prepare_files_multi_data.py
@@ -38,10 +38,10 @@ def main():
         os.system(cmd)
 
     # Check that all passed directories exist and print them.
-    for i in range(1, len(args_array)-1):
-        if not(os.path.isdir(args_array[i])):
-            sys.stderr.write('{} does not exist. Exiting...\n'.format(args_array[i]))
-            sys.exit()
+    # for i in range(1, len(args_array)-1):
+    #     if not(os.path.isdir(args_array[i])):
+    #         sys.stderr.write('{} does not exist. Exiting...\n'.format(args_array[i]))
+    #         sys.exit()
     sys.stderr.write('Output directory is {}\n'.format(out_dir))
     sys.stderr.write('Todo directory is {}\n'.format(todo_dir))
     sys.stderr.write('Data directory is {}\n'.format(data_dir))
