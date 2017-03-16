3d4bb3b1a49fe0141079d7c39f868a5fe5ec84c9
aszewciw <aszewci@gmail.com>
2017-03-16 13:30:53 -0500

    wrong fname

--- examples/test_jk/jk_errors.sh
+++ examples/test_jk/jk_errors.sh
@@ -5,4 +5,4 @@ bins_dir=/fs1/szewciw/MW_Structure/results/mcmc_100chains/prep_bins/;
 jk_dir=./data/;
 out_dir=./data/;
 
-python jk_mock.py $N_jk $bins_dir $jk_dir $out_dir
+python jk_errors.py $N_jk $bins_dir $jk_dir $out_dir
