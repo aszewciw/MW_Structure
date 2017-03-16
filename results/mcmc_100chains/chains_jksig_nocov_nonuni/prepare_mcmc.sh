081ff8c4fe54231bfcd20eef97c15fe6fc89d76d
aszewciw <aszewci@gmail.com>
2017-03-16 15:36:04 -0500

    prep and run files

---
+++ results/mcmc_100chains/chains_jksig_nocov_nonuni/prepare_mcmc.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+cfname='prep_cmd.txt';
+out_dir='./data/';
+data_dir='../prep_data_samps/data/';
+model_dir='../prep_model_nonuniform/data/';
+stats_dir='../prep_jk_errors/data/sample_0/';
+fid_dir=None;
+bins_dir='../prep_bins/';
+Ndata=100;
+cov=0;
+
+rm $cfname
+
+python prepare_mcmc.py $cfname $out_dir $data_dir $model_dir $stats_dir \
+$fid_dir $bins_dir $Ndata $cov
+bash $cfname
+
+# Add information about run
+info_file='prep_info.txt';
+rm $info_file
+INFO="prepare_mcmc.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
---
+++ results/mcmc_100chains/chains_jksig_nocov_nonuni/run_mcmc.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+cfname='mcmc_cmd.txt';
+indir='./data/';
+datdir='../prep_data_samps/data/';
+outdir='./out_data/';
+nprocs=8;
+max_s=100000;
+tol=0.0;
+Ndata=100;
+stats_dir='../prep_jk_errors/data/';
+
+rm $cfname
+
+python run_mcmc.py $cfname $indir $datdir $outdir $nprocs $max_s $tol $Ndata $stats_dir
+bash $cfname
+
+# Add information about run
+info_file='mcmc_info.txt';
+rm $info_file
+INFO="run_mcmc.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
