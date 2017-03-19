976dc4f5428eb24910941226b580afc0542ff8a7
aszewciw <aszewci@gmail.com>
2017-03-19 15:00:43 -0500

    running mcmc

--- results/mcmc_100chains/chains_jksig_nocov_nonuni/run_mcmc.py
+++ results/mcmc_100chains/chains_jksig_nocov_nonuni/run_mcmc.py
@@ -58,8 +58,7 @@ def main():
     with open(cfname, 'w') as f:
 
         for i in range(Ndata):
-            if i<2:
-                continue
+
             ofname = out_dir + 'results_' + str(i) + '.dat'
             dd_dir = data_dir + 'sample_' + str(i) + '/'
             std_dir = stats_dir + 'sample_' + str(i) + '/'
