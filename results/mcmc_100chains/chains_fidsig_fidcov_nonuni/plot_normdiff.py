e07794e2a5d7cb61b565ec806f66a54822dcad84
aszewciw <aszewci@gmail.com>
2017-03-20 10:02:49 -0500

    trying to also plot chi2

--- results/mcmc_100chains/chains_fidsig_fidcov_nonuni/plot_normdiff.py
+++ results/mcmc_100chains/chains_fidsig_fidcov_nonuni/plot_normdiff.py
@@ -20,6 +20,8 @@ def make_bins(x, bwidth):
     bin_min=int(min(x))-1
     bin_max=int(max(x))+1+bwidth
 
+    if bin_min
+
     bins=np.arange(bin_min,bin_max,bwidth)
 
     return bins
@@ -67,10 +69,53 @@ def main():
     bwidth=0.5
     spnum = 321
 
+    # for i in range(len(pd_keys)):
+    #     key = pd_keys[i]
+    #     if key=='chi2':
+    #         continue
+    #     plt.subplot(spnum+i)
+    #     bins = make_bins(STATS[key]['normdiff'], bwidth)
+    #     n, b, patches = plt.hist(STATS[key]['normdiff'], bins=bins,
+    #         facecolor='green', alpha=0.7, label=labels[i])
+    #     median = np.median(STATS[key]['normdiff'])
+    #     std_minus = np.percentile(STATS[key]['normdiff'], q=16)
+    #     std_plus = np.percentile(STATS[key]['normdiff'], q=84)
+    #     median_err_minus = (median-std_minus)/np.sqrt(Nfiles)
+    #     median_err_plus = (std_plus-median)/np.sqrt(Nfiles)
+    #     med_label = 'med='+str(np.round(median,2))
+    #     std_plt = np.round((std_plus-std_minus)/2.0, 2)
+    #     std_label = r'$\sigma$='+str(std_plt)
+    #     plt.axvline(median, color='r', linestyle='solid', label=med_label)
+    #     plt.axvline(std_minus, color='b', linestyle='solid', label=std_label)
+    #     plt.axvline(std_plus, color='b', linestyle='solid')
+    #     plt.axvline(median - median_err_minus, color='r', linestyle='--')
+    #     plt.axvline(median + median_err_plus, color='r', linestyle='--')
+    #     plt.axis([min(bins), max(bins), 0, 1.1*max(n)])
+    #     # plt.ylabel(labels[i])
+    #     if i==3 or i==4:
+    #         plt.xlabel(axis_label, fontsize=12)
+    #     # Decide where to place legend
+    #     left = median - np.min(STATS[key]['normdiff'])
+    #     right = np.max(STATS[key]['normdiff']) - median
+    #     if left>right:
+    #         loc='upper left'
+    #     else:
+    #         loc='upper right'
+    #     plt.legend(loc=loc, fontsize=6)
+    #     sys.stderr.write('Key: {}, min: {}, max: {}\n'.format(key, np.min(STATS[key]['normdiff']), np.max(STATS[key]['normdiff']) ) )
+
+    # plt.savefig(data_dir + 'normdiff' + '.png')
+
+
+
+
+
     for i in range(len(pd_keys)):
         key = pd_keys[i]
         if key=='chi2':
-            continue
+            bwidth = 20
+        else:
+            bwdith = 0.5
         plt.subplot(spnum+i)
         bins = make_bins(STATS[key]['normdiff'], bwidth)
         n, b, patches = plt.hist(STATS[key]['normdiff'], bins=bins,
@@ -89,9 +134,7 @@ def main():
         plt.axvline(median - median_err_minus, color='r', linestyle='--')
         plt.axvline(median + median_err_plus, color='r', linestyle='--')
         plt.axis([min(bins), max(bins), 0, 1.1*max(n)])
-        print(min(bins), max(bins))
-        # plt.ylabel(labels[i])
-        if i==3 or i==4:
+        if i==3 or i==5:
             plt.xlabel(axis_label, fontsize=12)
         # Decide where to place legend
         left = median - np.min(STATS[key]['normdiff'])
