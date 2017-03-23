97dfa526c425cfcbcf94a50ce11ff87a38de1fae
aszewciw <aszewci@gmail.com>
2017-03-23 12:12:52 -0500

    title

--- results/error_analysis/check_Gaussianity/check_gaussianity.py
+++ results/error_analysis/check_Gaussianity/check_gaussianity.py
@@ -89,8 +89,12 @@ def main():
     png_list=[]
 
     for i in range(N_los):
+
         ID = ID_list[i]
         sys.stderr.write('On pointing {}\n'.format(ID))
+
+        if ID!='27' and ID!='30':
+            continue
 
         for j in range(N_mocks):
 
@@ -102,6 +106,7 @@ def main():
 
         plt.clf()
         fig1=plt.figure(1)
+        plt.title('Pair Histograms from 10000 mocks, l.o.s. ' + ID)
         spnum = 0
         bwidth=1
 
@@ -124,71 +129,6 @@ def main():
 
     gif_name= out_dir + 'pair_hist.gif'
     GIF_MOVIE(png_list, gif_name, delay=120)
-
-
-
-
-    # # loop over each l.o.s.
-    # for p in todo_list:
-
-    #     if p.ID != '27':
-    #         continue
-
-    #     sys.stderr.write('Making histogram for pointing {}\n'.format(p.ID))
-
-    #     # Empty arrays where each row is counts for 1 mock in current l.o.s.
-    #     DD_raw_all = np.zeros((N_mocks, N_bins))
-    #     DD_all     = np.zeros((N_mocks, N_bins))
-
-    #     # loop over all mocks and add DD values to array
-    #     for i in range(N_mocks):
-
-    #         # Load counts for a single mock
-    #         corr_file = ( data_dir + 'mock_' + str(i+1) + '/mock_pairs_'
-    #             + p.ID + '.dat' )
-    #         DD_raw_all[i], DD_all[i] = np.genfromtxt( corr_file, unpack=True,
-    #             usecols=[4, 5] )
-
-    #     # Load mean and standard deviation
-    #     stats_file = stats_dir + 'stats_' + p.ID + '.dat'
-    #     mu_list, sigma_list = np.genfromtxt(stats_file, unpack=True,
-    #                             usecols=[0,2])
-
-    #     # Plot histogram for each bin
-    #     for i in range(N_bins):
-
-    #         # get current array for hist
-    #         DD = DD_all[:, i]
-
-    #         # Make histogram bins
-    #         hist_min  = min(DD)
-    #         hist_max  = max(DD)
-    #         offset    = 0.001*hist_max
-    #         hist_max  += offset # make sure max is in a bin
-    #         N_hist    = 30
-    #         hist_bins = np.linspace(hist_min, hist_max, num=50)
-
-    #         # Get normalized counts, bin edges, bin centers
-    #         counts, edges = np.histogram(DD, hist_bins, normed=True)
-    #         binWidth      = edges[1] - edges[0]
-    #         centers       = edges[:-1]+0.5*(edges[1:]-edges[:-1])
-
-    #         # Plot bar graph with transparency
-    #         # Multiply by binWidth because np.histogram divides by it by default
-    #         plt.clf()
-    #         plt.figure(1)
-    #         plt.bar(centers, counts*binWidth, binWidth, color='blue', alpha=0.1)
-
-    #         # Add a normal curve on top of the histogram
-    #         mu    = mu_list[i]
-    #         sigma = sigma_list[i]
-    #         x     = np.linspace(centers[0], centers[-1], N_hist)
-    #         plt.plot(x, norm.pdf(x, mu, sigma)*binWidth, color='r')
-
-    #         # Save figure
-    #         figure_name = ( plots_dir + 'histogram_' + p.ID + 'bin_' + str(i)
-    #                         + '.png' )
-    #         plt.savefig(figure_name)
 
 
 if __name__ == '__main__':
--- results/error_analysis/check_Gaussianity/make_gifs.sh
+++ results/error_analysis/check_Gaussianity/make_gifs.sh
@@ -5,4 +5,7 @@ pairs_dir=/fs1/szewciw/MW_Structure/results/error_analysis/data_pairs/;
 out_dir=./plots/;
 bins_dir=../prep_bins/;
 
+rm -rf $out_dir
+mkdir $out_dir
+
 python check_gaussianity.py $N_mocks $pairs_dir $out_dir $bins_dir
