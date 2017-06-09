63feea01495b8f7e319b6f79060b0319ffbf007c
aszewciw <aszewci@gmail.com>
2017-06-09 15:28:12 -0500

    iji

---
+++ results/test_ridiculous_params/test_mcmc/run_mcmc.py
@@ -0,0 +1,71 @@
+'''
+A sample script to run an mcmc chain. We assume that all of the data has been
+prepared in the "in_dir" folder. It outputs a file containing executable commands
+used in the mock creations. This script is called by a bash script "test_mcmc.sh"
+where passed parameters are specified.
+'''
+import mw_utilities_python as mwu
+import sys, os
+import numpy as np
+
+def main():
+
+    # get command filename, out_dir, nprocs, nmocks
+    elements_needed = int(14)
+    args_array      = np.array(sys.argv)
+    N_args          = len(args_array)
+    assert(N_args == elements_needed)
+    cfname   = args_array[1]
+    in_dir   = args_array[2]
+    data_dir = args_array[3]
+    out_dir  = args_array[4]
+    nprocs   = args_array[5]
+    max_s    = args_array[6]
+    tol      = args_array[7]
+    cov      = args_array[8]
+    rn       = args_array[9]
+    zn       = args_array[10]
+    rk       = args_array[11]
+    zk       = args_array[12]
+    a        = args_array[13]
+
+    # get directories of scripts and executables
+    todo_dir = mwu.get_path.get_cleandata_path()
+    scripts_dir = mwu.get_path.get_scripts_path()
+    mcmc_dir = scripts_dir + 'mcmc/mcmc_mpi/'
+    exe_file = mcmc_dir + 'bin/run_mcmc'
+
+    # Check for dir/file existence
+    if not os.path.isdir(in_dir):
+        sys.stderr.write('{} does not exist. Please make dir and fill with data.\n'.format(in_dir))
+        sys.exit()
+
+    if not os.path.isdir(out_dir):
+        sys.stderr.write('{} does not exist. Making...\n'.format(out_dir))
+        cmd = 'mkdir ' + out_dir
+        os.system(cmd)
+
+    if not os.path.isdir(data_dir):
+        sys.stderr.write('{} does not exist. Please make dir and fill with data.\n'.format(data_dir))
+        sys.exit()
+
+    if not os.path.isfile(exe_file):
+        sys.stderr.write('{} does not exist. Making...\n'.format(exe_file))
+        cmd = 'make -C ' + mcmc_dir
+        os.system(cmd)
+
+    # Write commands to file
+    with open(cfname, 'w') as f:
+
+        cmd = (
+            'time mpirun -n ' + nprocs + ' ' + exe_file + ' -fn ' + ofname
+            + ' -l_id ' + str(len(in_dir)) + ' -id ' + in_dir + ' -max_s ' + max_s
+            + ' -tol ' + tol + ' -dd ' + dd_dir + ' -l_dd ' + str(len(dd_dir))
+            + ' -cov ' + cov + ' -rn ' + rn + ' -zn ' + zn + ' -rk ' + rk
+            + ' -zk ' + zk + ' -a ' + a
+            )
+        f.write(cmd)
+        f.write('\n')
+
+if __name__ == '__main__':
+    main()
---
+++ results/test_ridiculous_params/test_mcmc/run_mcmc.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+cfname='mcmc_cmd.txt';
+indir='/fs1/szewciw/MW_Structure/results/mcmc_100chains/chains_fidsig_fidcov_nonuni/data/';
+datdir='./data/';
+outdir='./out_data/';
+nprocs=16;
+max_s=10;
+tol=0.0;
+cov=1;
+rn=69.627;
+zn=0.259;
+rk=4.881;
+zk=0.691;
+a=0.736;
+
+rm $cfname
+
+python run_mcmc.py $cfname $indir $datdir $outdir $nprocs $max_s $tol $cov $rn $zn $rk $zk $a
+bash $cfname
+
+# Add information about run
+info_file='mcmc_info.txt';
+rm $info_file
+INFO="run_mcmc.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
