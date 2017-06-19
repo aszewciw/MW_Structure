9440ed22945631ce291a7264e01a0d7d6bd6d938
aszewciw <aszewci@gmail.com>
2017-06-19 12:54:57 -0500

    random catalog

---
+++ results/mcmc_100chains/prep_randoms/make_uniform.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+cfname=uniform_cmd.txt;
+out_dir=./data/;
+num_ratio=10; # N_uniform / N_data
+
+rm -rf $out_dir
+mkdir $out_dir
+
+python make_uniform.py $cfname $out_dir $num_ratio
+bash $cfname
+
+# Add information about run
+info_file=uniform_info.txt;
+rm $info_file
+INFO="make_uniform.sh was most recently run on:";
+echo $INFO > $info_file
+date >> $info_file
+
+echo Time of run output to $info_file
