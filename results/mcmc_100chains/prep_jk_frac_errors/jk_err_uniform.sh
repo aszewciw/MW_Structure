1bbf5e4b9ec70926ce782784e6dffc6ecedb5544
aszewciw <aszewci@gmail.com>
2017-07-23 19:16:34 -0500

    ojasdfjiop

---
+++ results/mcmc_100chains/prep_jk_frac_errors/jk_err_uniform.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+N_jk=10;
+bins_dir=/fs1/szewciw/MW_Structure/results/mcmc_100chains/prep_bins/;
+jk_dir=./data_uniform/;
+out_dir=./data_uniform/;
+
+python jk_errors.py $N_jk $bins_dir $jk_dir $out_dir
