89fce65d573195fbf9178998408e688630a052e3
aszewciw <aszewci@gmail.com>
2017-07-23 19:03:54 -0500

    fname

--- results/mcmc_100chains/prep_jk_frac_errors/jk_err_mocks.sh
+++ results/mcmc_100chains/prep_jk_frac_errors/jk_err_mocks.sh
@@ -6,4 +6,4 @@ bins_dir=/fs1/szewciw/MW_Structure/results/mcmc_100chains/prep_bins/;
 jk_dir=./data_mocks/;
 out_dir=./data_mocks/;
 
-python jk_error_mocks.py $N_jk $bins_dir $jk_dir $out_dir $Nsamp
+python jk_err_mocks.py $N_jk $bins_dir $jk_dir $out_dir $Nsamp
